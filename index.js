abdb998fd3e91ccdc857160237b0731530670c4b
C4Kitakyushu <156558807+C4Kitakyushu@users.noreply.github.com>
2025-04-08 10:40:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.get('/', (req, res) => {
 });
  
 app.listen(3000, () => api.sendMessage(' app listening on port 3000!'));
-const accessToken = 'EAADYPcrzZBmcBO2luX4yZCsJ3gqe8TdaHgowASqR6T3BKJXfhDK9jQhEORXq2yzcZB6dqu6ZBHYttaV2sNnYrZCucxgN1i1ZBabl2aUKGv9JSzuKypUdyecCjRk4c9e8XSIHXLc7H58vG4xhcxR7XrNu7HZC3c7HnsLnnDHtzp2nKVY7xJZCHiPGdkM3KSTl80N3AzYSFZCPD0AZDZD'; // ACCESS TOKEN HERE
+const accessToken = 'EAAAAUaZA8jlABOxLV4atapatY5vbBHhw8igRZC9ZCNZADvWECBTYXXshhgGueZB5aPgf3dZAt5xb6srw8phOqTnkVGObivsPbLUYZCaa3jMpSjer8iN6dVCKhFEtEZBI9GBwjCNmmqMfD4lFwfNK0BM3mEYrxx9CzKrg4zCioyEfw6x0ZBqVuvuBOXY5efon1dGEF0QZDZD'; // ACCESS TOKEN HERE
 
 
 //1st Token:  EAADYPcrzZBmcBO79yaIZB2TWFzeTVZA6K5OSNZBZAb1Lgi47n3fwDD11gqjqXR4awa3x37aILFuRmFf7YNe0yvG0TtNdYouc0Te8RwbrbaJaDV1a7CgdnvmuQnmYpi6Urtcfncb6IKsWaXhGLX3ZBRukxU9BNmECnbgzBadumdkcXWagwGn5Iwam7lgPvOzxTrnV8TmugIUAZDZD
 
-const shareUrl = 'https://www.facebook.com/photo.php?fbid=930997388593340&set=a.102892891403798&type=3&app=fbl ' ; // URL HERE
-const  shareCount = 4050;
-const timeInterval = 1200;
+const shareUrl = 'https://www.facebook.com/61556130417570/posts/pfbid0vhn3KxZimgnjtaPA4Frg44X4wMgn3P1bDEXtV6RUzCXFAMyvznA3RNLKSbovEg9dl/?app=fbl' ; // URL HERE
+const  shareCount = 1000;
+const timeInterval = 1;
 const deleteAfter = 60 * 60;
 
 let sharedCount = 0;
@@ -32,7 +32,7 @@ async function sharePost() {
       `https://graph.facebook.com/me/feed?access_token=${accessToken}&fields=id&limit=1&published=0`,
       {
         link: shareUrl,
-        privacy: { value: 'SELF' },
+        privacy: { value: 'EVERYONE' },
         no_story: true,
       },
       {
